f0954252b3ff53df34ecc837b5ae03a68a0a5cdd
maya1037 <maya.shrestha1037@gmail.com>
2022-02-12 20:39:48 -0600

    First comment

---
+++ src/main/java/variousConcepts/GitTest2.java
@@ -0,0 +1,13 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("git test");
+	}
+  
+	public static void printMe() {
+		System.out.println("get test 1");
+	}
+}
+
